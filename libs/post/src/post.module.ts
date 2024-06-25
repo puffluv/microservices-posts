5378115b3836d850063e0a893b82f0596fea0c80
puffluv <alibihasenov@gmail.com>
2024-06-25 20:26:53 +0700

    Add: post facade factory

--- libs/post/src/post.module.ts
+++ libs/post/src/post.module.ts
@@ -6,25 +6,31 @@ import { POST_COMMANDS_HANDLERS } from './application-services/commands';
 import { POST_QUERIES_HANDLERS } from './application-services/queries';
 import { POST_EVENTS_HANDLERS } from './application-services/events';
 import { PostFacade } from './application-services';
+import { postFacadeFactory } from './providers/post-facade.factory';
 
 @Module({
-    imports: [CqrsModule, TypeOrmModule.forFeature([PostEntity])],
-    providers: [
-        ...POST_COMMANDS_HANDLERS,
-        ...POST_QUERIES_HANDLERS,
-        ...POST_EVENTS_HANDLERS,
-    ],
-    exports: [PostFacade],
+  imports: [CqrsModule, TypeOrmModule.forFeature([PostEntity])],
+  providers: [
+    ...POST_COMMANDS_HANDLERS,
+    ...POST_QUERIES_HANDLERS,
+    ...POST_EVENTS_HANDLERS,
+    {
+      provide: PostFacade,
+      inject: [CommandBus, QueryBus, EventBus],
+      useFactory: postFacadeFactory,
+    },
+  ],
+  exports: [PostFacade],
 })
 export class PostModule implements OnModuleInit {
-    constructor(
-        private readonly commandBus: CommandBus,
-        private readonly queryBus: QueryBus,
-        private readonly eventBus: EventBus,
-    ) {}
-    onModuleInit() {
-        this.commandBus.register(POST_COMMANDS_HANDLERS);
-        this.queryBus.register(POST_QUERIES_HANDLERS);
-        this.eventBus.register(POST_EVENTS_HANDLERS);
-    }
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+    private readonly eventBus: EventBus,
+  ) {}
+  onModuleInit() {
+    this.commandBus.register(POST_COMMANDS_HANDLERS);
+    this.queryBus.register(POST_QUERIES_HANDLERS);
+    this.eventBus.register(POST_EVENTS_HANDLERS);
+  }
 }
