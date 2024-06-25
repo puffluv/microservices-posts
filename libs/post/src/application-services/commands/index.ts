513addfbf0fb8a02e926595798e7f5e29077fc60
puffluv <alibihasenov@gmail.com>
2024-06-25 19:58:23 +0700

    Upd: facade. Add commands and queries

--- libs/post/src/application-services/commands/index.ts
+++ libs/post/src/application-services/commands/index.ts
@@ -1,13 +1,25 @@
-import { Type } from "@nestjs/common";
-import { ICommandHandler } from "@nestjs/cqrs";
-import { CreatePostCommandHandler } from "./create-post/create.post.command-handler";
-import { UpdatePostCommandHandler } from "./update-post/update-post.command-handler";
-import { DeletePostCommandHandler } from "./delete-post/delete-post.command-handler";
-import { SetPublishedCommandHandler } from "./set-published/set-published.command-handler";
+import { Type } from '@nestjs/common';
+import { ICommandHandler } from '@nestjs/cqrs';
+import { CreatePostCommandHandler } from './create-post/create.post.command-handler';
+import { UpdatePostCommandHandler } from './update-post/update-post.command-handler';
+import { DeletePostCommandHandler } from './delete-post/delete-post.command-handler';
+import { SetPublishedCommandHandler } from './set-published/set-published.command-handler';
+
+// commands
+export * from './create-post/create.post.command';
+export * from './delete-post/delete-post.command';
+export * from './set-published/set-published.command';
+export * from './update-post/update-post.command';
+
+// command-handlers
+export * from './create-post/create.post.command-handler';
+export * from './delete-post/delete-post.command-handler';
+export * from './set-published/set-published.command-handler';
+export * from './update-post/update-post.command-handler';
 
 export const POST_COMMANDS_HANDLERS: Type<ICommandHandler>[] = [
-    CreatePostCommandHandler,
-    UpdatePostCommandHandler,
-    DeletePostCommandHandler,
-    SetPublishedCommandHandler,
+  CreatePostCommandHandler,
+  UpdatePostCommandHandler,
+  DeletePostCommandHandler,
+  SetPublishedCommandHandler,
 ];
--- libs/post/src/application-services/queries/index.ts
+++ libs/post/src/application-services/queries/index.ts
@@ -1,9 +1,17 @@
-import { Type } from "@nestjs/common";
-import { IQueryHandler } from "@nestjs/cqrs";
-import { GetPostQueryHandler } from "./get-post/get-post.query-handler";
-import { GetPostsQueryHandler } from "./get-posts/get-posts.query-handler";
+import { Type } from '@nestjs/common';
+import { IQueryHandler } from '@nestjs/cqrs';
+import { GetPostQueryHandler } from './get-post/get-post.query-handler';
+import { GetPostsQueryHandler } from './get-posts/get-posts.query-handler';
+
+// queries
+export * from './get-post/get-post.query';
+export * from './get-posts/get-posts.query';
+
+// queries-handlers
+export * from './get-post/get-post.query-handler';
+export * from './get-posts/get-posts.query-handler';
 
 export const POST_QUERIES_HANDLERS: Type<IQueryHandler>[] = [
-    GetPostQueryHandler,
-    GetPostsQueryHandler,
+  GetPostQueryHandler,
+  GetPostsQueryHandler,
 ];
