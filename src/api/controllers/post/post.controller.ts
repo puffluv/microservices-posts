76fdbc010a378d3f932dab58f346c1bb83bc2169
puffluv <alibihasenov@gmail.com>
2024-06-27 13:17:50 +0700

    Add: post method: create post

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,7 +1,22 @@
 import { PostFacade } from '@lib/post/application-services';
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePostDto } from './dto';
+import { CurrentUser, ICurrentUser } from '@lib/auth';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postFacade: PostFacade) {}
+
+  @Post()
+  createPost(
+    @CurrentUser() user: ICurrentUser,
+    @Body() createPostDto: CreatePostDto,
+  ) {
+    return this.postFacade.commands.createPost({
+      ...createPostDto,
+      authorId: uuidv4(),
+      // authorId: user.userId,
+    });
+  }
 }
