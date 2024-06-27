0c0806e6d97f8a8497f44a51e803d653d5d1c7b5
puffluv <alibihasenov@gmail.com>
2024-06-27 18:10:29 +0700

    Add: get one post

--- libs/auth/src/decorators/public.decorator.ts
+++ libs/auth/src/decorators/public.decorator.ts
@@ -1,8 +1,7 @@
-import { ExecutionContext, SetMetadata } from '@nestjs/common';
+import { SetMetadata, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 
 export const PUBLIC_KEY = 'public';
-
 export const Public = () => SetMetadata(PUBLIC_KEY, true);
 
 export const isPublic = (ctx: ExecutionContext, reflector: Reflector) => {
--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,11 +1,19 @@
 import { PostFacade } from '@lib/post/application-services';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CreatePostDto } from './dto';
-import { CurrentUser, ICurrentUser } from '@lib/auth';
+import { CurrentUser, ICurrentUser, Public } from '@lib/auth';
 import { v4 as uuidv4 } from 'uuid';
 import { JwtGuard } from '@lib/auth/guards/jwt.guard';
 
-@UseGuards(JwtGuard)
+// @UseGuards(JwtGuard)
 @Controller('post')
 export class PostController {
   constructor(private readonly postFacade: PostFacade) {}
@@ -17,7 +25,14 @@ export class PostController {
   ) {
     return this.postFacade.commands.createPost({
       ...createPostDto,
-      authorId: user.userId,
+      authorId: uuidv4(),
+      // authorId: user.userId,
     });
+  }
+
+  @Public()
+  @Get(':id')
+  getPostById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.postFacade.queries.getOnePost(id);
   }
 }
