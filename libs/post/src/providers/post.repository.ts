dbbbd2e85b7af124cba6bd0414b9bb540b0d0abb
puffluv <alibihasenov@gmail.com>
2024-06-27 13:17:03 +0700

    upd

--- libs/post/src/providers/post.repository.ts
+++ libs/post/src/providers/post.repository.ts
@@ -1,9 +1,11 @@
-import { PaginationDto } from "@lib/shared/dto";
-import { IPost, PostAggregate } from "../domain"
+import { PaginationDto } from '@lib/shared/dto';
+import { IPost, PostAggregate } from '../domain';
 
 export abstract class PostRepository {
-    abstract save(post: IPost): Promise<PostAggregate>; 
-    abstract findOne(id: string): Promise<PostAggregate | null>;
-    abstract findAll(pagination: PaginationDto): Promise<[PostAggregate[], number]>;
-    abstract delete(id: string): Promise<boolean>;
+  abstract save(post: IPost): Promise<PostAggregate>;
+  abstract findOne(id: string): Promise<PostAggregate | null>;
+  abstract findAll(
+    pagination: PaginationDto,
+  ): Promise<[PostAggregate[], number]>;
+  abstract delete(id: string): Promise<boolean>;
 }
