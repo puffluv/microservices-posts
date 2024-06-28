105f511dba580c20c2d592b7bc79edf390fc2ffa
puffluv <alibihasenov@gmail.com>
2024-06-28 14:44:34 +0700

    Add: resolver.Query get one Post

---
+++ src/api/resolvers/post/post.resolver.ts
@@ -0,0 +1,13 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { PostResponse } from '../responses/post.response';
+import { PostFacade } from '@lib/post/application-services';
+
+@Resolver(() => PostResponse)
+export class PostResolver {
+  constructor(private readonly postFacade: PostFacade) {}
+
+  @Query(() => PostResponse, { name: 'post' })
+  async getPostById(@Args('id') id: string) {
+    return this.postFacade.queries.getOnePost(id);
+  }
+}
