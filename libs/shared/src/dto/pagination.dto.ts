443ecee1e3de00d137e49df7bcda56416ebbba66
puffluv <alibihasenov@gmail.com>
2024-06-28 17:37:26 +0700

    Upd

--- libs/shared/src/dto/pagination.dto.ts
+++ libs/shared/src/dto/pagination.dto.ts
@@ -1,16 +1,23 @@
-import { Type } from "class-transformer";
-import { IsNumber, IsOptional, IsPositive, Min } from "class-validator";
+import { ArgsType, Field, Int } from '@nestjs/graphql';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
 
+@ArgsType()
 export class PaginationDto {
-    @IsOptional()
-    @Min(0)
-    @Type(() =>  Number)
-    @IsNumber({ allowNaN: false, allowInfinity: false})
-    offset = 0;
+  @ApiPropertyOptional({ description: 'Пропуск строк', type: 'number' })
+  @IsOptional()
+  @Min(0)
+  @IsNumber({ allowNaN: false, allowInfinity: false })
+  @Type(() => Number)
+  @Field(() => Int, { description: 'Пропуск строк' })
+  offset = 0;
 
-    @IsOptional()
-    @Type(() =>  Number)
-    @IsNumber({ allowNaN: false, allowInfinity: false})
-    @IsPositive()
-    limit = 15;
+  @ApiPropertyOptional({ description: 'Количество строк', type: 'number' })
+  @IsOptional()
+  @IsNumber({ allowNaN: false, allowInfinity: false })
+  @Type(() => Number)
+  @IsPositive()
+  @Field(() => Int, { description: 'Количество строк' })
+  limit = 15;
 }
--- src/api/resolvers/post/post.resolver.ts
+++ src/api/resolvers/post/post.resolver.ts
@@ -1,6 +1,9 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { PostResponse } from '../responses/post.response';
+import { PostResponse, PaginatedPosts } from '../responses';
 import { PostFacade } from '@lib/post/application-services';
+import { PaginationDto } from '@lib/shared';
+import { plainToInstance } from 'class-transformer';
+import { Public } from '@lib/auth';
 
 @Resolver(() => PostResponse)
 export class PostResolver {
@@ -9,5 +12,18 @@ export class PostResolver {
   @Query(() => PostResponse, { name: 'post' })
   async getPostById(@Args('id') id: string) {
     return this.postFacade.queries.getOnePost(id);
+  }
+
+  @Public()
+  @Query(() => PaginatedPosts, { name: 'posts' })
+  async getPosts(@Args() paginationDto: PaginationDto) {
+    const pagination = plainToInstance(PaginationDto, paginationDto);
+    // @ts-ignore
+    const [data, total] = await this.postFacade.queries.getAllPosts(pagination);
+    return {
+      ...pagination,
+      data,
+      total,
+    };
   }
 }
